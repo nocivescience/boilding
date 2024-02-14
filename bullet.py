6ef2cf05c45765dab8adcfe8753d39aa7083fddf
nocive.science <nocive.science@gmail.com>
2024-02-14 00:29:26 -0300

    ya aparece volteado

--- bullet.py
+++ bullet.py
@@ -1,4 +1,5 @@
 from manim import *
+import itertools as it
 
 class Bullet(Triangle):
     CONFIG = {
@@ -9,11 +10,12 @@ class Bullet(Triangle):
         "aspect": 1.5,
     }
 
-    def __init__(self, **kwargs):
+    def __init__(self, color, **kwargs):
         super().__init__(**kwargs)
         self.stretch_to_fit_height(self.CONFIG['length'])
         self.stretch_to_fit_width(self.CONFIG['length'] * self.CONFIG['aspect'])
-        self.points[5:7] += np.array([self.CONFIG['length'], 0, 0])
+        self.points[5:7] += np.array([.1, 0, 0])
+        self.set_color(color)
         self.scale(0.5)
 
     def get_angle(self):
@@ -23,28 +25,18 @@ class Bullet(Triangle):
         return self.point_from_proportion(0.5) - self.get_start()
 
 class TestBullet(Scene):
+    CONFIG={
+        'colores': [RED, GREEN, BLUE, YELLOW, ORANGE, PINK, TEAL, PURPLE, GREY]
+    }
     def construct(self):
         vqueue1 = VGroup()
-        vqueue2 = VGroup()
         self.an = 0
-
+        colores = it.cycle(self.CONFIG['colores'])
         def update_bullet1(obj, dt):
-            obj.add(Bullet().scale(0.7).rotate((2) * 10 + self.an * 10))
+            obj.add(Bullet(color = next(colores)).scale(0.7).rotate((2) * 10 + self.an * 10))
             for k in obj:
                 k.shift(dt * 2 * np.array([
-                    np.cos(k.get_angle()),
-                    np.sin(k.get_angle()),
-                    0
-                ]))
-                if abs(np.linalg.norm(k.get_center() - ORIGIN)) > 7:
-                    obj.remove(k)
-            self.an += 1 * DEGREES
-
-        def update_bullet2(obj, dt):
-            obj.add(Bullet().scale(0.7).rotate(PI + (2) * 10 + self.an * 10))
-            for k in obj:
-                k.shift(dt * 2 * np.array([
-                    np.cos(k.get_angle()),
+                    np.cos(k.get_angle())+np.sin(k.get_angle()),
                     np.sin(k.get_angle()),
                     0
                 ]))
@@ -53,9 +45,8 @@ class TestBullet(Scene):
             self.an += 1 * DEGREES
 
         vqueue1.add_updater(update_bullet1)
-        vqueue2.add_updater(update_bullet2)
-        self.add(vqueue1,vqueue2)
-        self.wait(3)
+        self.add(vqueue1)
+        self.wait(20)
 
 
 # from manim import *
